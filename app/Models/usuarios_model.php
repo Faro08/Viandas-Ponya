13802579213b3ed774df0fdde6a6de088998780d
Faro note pramone <farid.gpasi@gmail.com>
2023-05-05 17:08:05 -0300

    usuarios model

---
+++ app/Models/usuarios_model.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class usuarios_model extends Model 
+{
+    protected $table = 'usuarios';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['nombre', 'apellido', 'usuario', 'email', 'pass',
+        , 'perfil_id', 'baja', 'created_at'];
+    
+}
