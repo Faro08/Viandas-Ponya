36ad0764a8c1667c668381b0039a2ce6daae01b6
Faro note pramone <farid.gpasi@gmail.com>
2023-06-07 17:16:16 -0300

    wip navbar user conditionals

--- app/Views/Front/footer_view.php
+++ app/Views/Front/footer_view.php
@@ -5,33 +5,34 @@
 
             <div class="col p-4 pb-0 text-start">
                 <div class="container d-flex p-1"><img class="icon-color-white me-3"
-                        src="assets/bootstrap-icons-1.10.4/geo-alt-fill.svg" alt="icono localizacion" width="25"
-                        height="25">
+                        src="<?php echo base_url("assets/bootstrap-icons-1.10.4/geo-alt-fill.svg");?>"
+                        alt="icono localizacion" width="25" height="25">
                     <p>Rivadavia 632, Corrientes - Capital</p>
                 </div>
                 <div class="container d-flex p-1"><img class="icon-color-white me-3"
-                        src="assets/bootstrap-icons-1.10.4/telephone-fill.svg" alt="icono telefono" width="25"
-                        height="25">
+                        src="<?php echo base_url("assets/bootstrap-icons-1.10.4/telephone-fill.svg");?>"
+                        alt="icono telefono" width="25" height="25">
                     <p>3794 - 123456</p>
                 </div>
                 <div class="container d-flex p-1"><img class="icon-color-white me-3"
-                        src="assets/bootstrap-icons-1.10.4/clock-history.svg" alt="icono reloj" width="25" height="25">
+                        src="<?php echo base_url("assets/bootstrap-icons-1.10.4/clock-history.svg");?>"
+                        alt="icono reloj" width="25" height="25">
                     <p><b>LUN - DOM</b> 11:00 - 23:00</p>
                 </div>
             </div>
             <div class="col p-4 pb-0">
-                <img src="assets/img/logo-viandas-ponya-verde-blanco.png" height="40">
+                <img src="<?php echo base_url("assets/img/logo-viandas-ponya-verde-blanco.png");?>" height="40">
                 <p>Platos y viandas frescas</p>
                 <div class="mb-4">
                     <a class="m-3" href="#!" role="button"><img class="icon-color-white"
-                            src="assets/bootstrap-icons-1.10.4/facebook.svg" alt="icono facebook" width="25"
-                            height="25"></i></a>
+                            src="<?php echo base_url("assets/bootstrap-icons-1.10.4/facebook.svg");?>"
+                            alt="icono facebook" width="25" height="25"></i></a>
                     <a class="m-3" href="#!" role="button"><img class="icon-color-white"
-                            src="assets/bootstrap-icons-1.10.4/instagram.svg" alt="icono instagram" width="25"
-                            height="25"></i></a>
+                            src="<?php echo base_url("assets/bootstrap-icons-1.10.4/instagram.svg");?>"
+                            alt="icono instagram" width="25" height="25"></i></a>
                     <a class="m-3" href="#!" role="button"><img class="icon-color-white"
-                            src="assets/bootstrap-icons-1.10.4/whatsapp.svg" alt="icono whatsapp" width="25"
-                            height="25"></i></a>
+                            src="<?php echo base_url("assets/bootstrap-icons-1.10.4/whatsapp.svg");?>"
+                            alt="icono whatsapp" width="25" height="25"></i></a>
                 </div>
             </div>
         </div>
@@ -39,6 +40,7 @@
 </footer>
 <!-- <script src="assets/js/bootstrap.bundle.min.js"></script> -->
 <script src="<?php echo base_url("assets/js/bootstrap.bundle.min.js");?>"></script>
+
 </body>
 
 </html>
--- app/Views/Front/nav_view.php
+++ app/Views/Front/nav_view.php
@@ -6,22 +6,63 @@
 
             <div class="container-fluid nav-container">
                 <a class="navbar-brand" href="<?php echo base_url('');?>">
-                    <img class="img-fluid header-logo" src="assets/img/logo-viandas-ponya-verde-blanco.png"
+                    <img class="img-fluid header-logo"
+                        src=<?php echo base_url ("assets/img/logo-viandas-ponya-verde-blanco.png");?>
                         alt="Logo Viandas Ponya" />
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                <!-- ADMIN -->
                 <?php if (session()->perfil_id ==1): ?>
-                <div class="btn btn-danger active  btnUser btn-sm">
+                <!-- <div class="btn btn-danger active  btnUser btn-sm">
                     <a href="">ADMIN:<?php echo session('nombre'); ?> </a>
+                </div> -->
+                <div class="collapse navbar-collapse " id="navbarNav">
+                    <ul class="navbar-nav ms-auto text-white ">
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url('');?>">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url('about');?>">Productos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url('commerce');?>">Clientes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url('contact');?>">Ventas</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                <img class="icon-color-white"
+                                    src="<?php echo base_url("assets/bootstrap-icons-1.10.4/person-circle.svg");?>"
+                                    alt="icono usuario" width="25" height="25">
+                            </a>
+                            <!--  <a class="nav-item dropdown-toggle" role="button"><img class="icon-color-white"
+                                    src="assets/bootstrap-icons-1.10.4/person-fill.svg" alt="icono facebook" width="25"
+                                    height="25" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i></a> -->
+
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <p class="dropdown-item">Administrador: <?php echo session('nombre'); ?></p>
+                                </li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="<?php echo base_url('logout');?>">Salir</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
+
+
+                <!-- USUARIO -->
                 <?php elseif(session()->perfil_id == 2): ?>
-                <div class="btn btn-danger active  btnUser btn-sm">
-                    <a href="">USUARIO:<?php echo session('nombre'); ?> </a>
-                </div>
-                <?php endif; ?>
                 <div class="collapse navbar-collapse " id="navbarNav">
                     <ul class="navbar-nav ms-auto text-white ">
 
@@ -63,7 +104,84 @@
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                                 aria-expanded="false">
-                                <img class="icon-color-white" src="assets/bootstrap-icons-1.10.4/person-circle.svg"
+                                <img class="icon-color-white"
+                                    src="<?php echo base_url("assets/bootstrap-icons-1.10.4/person-circle.svg");?>"
+                                    alt="icono usuario" width="25" height="25">
+                            </a>
+                            <!--  <a class="nav-item dropdown-toggle" role="button"><img class="icon-color-white"
+                                    src="assets/bootstrap-icons-1.10.4/person-fill.svg" alt="icono facebook" width="25"
+                                    height="25" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i></a> -->
+
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <p class="dropdown-item">Usuario: <?php echo session('nombre'); ?></p>
+                                </li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="<?php echo base_url('login');?>">Mis compras</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="<?php echo base_url('logout');?>">Salir</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url('contact');?>"><img class="icon-color-white"
+                                    src="<?php echo base_url("assets/bootstrap-icons-1.10.4/cart.svg");?>"
+                                    alt="icono carrito" width="25" height="25">
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+
+                <!-- VISITANTE -->
+                <?php else: ?>
+                <div class="collapse navbar-collapse " id="navbarNav">
+                    <ul class="navbar-nav ms-auto text-white ">
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url('');?>">Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Plato del dia
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="<?php echo base_url('products');?>">Carne</a></li>
+                                <li><a class="dropdown-item" href="<?php echo base_url('wip');?>">Vegetariano</a></li>
+                                <li><a class="dropdown-item" href="<?php echo base_url('wip');?>">Saludable</a></li>
+
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Viandas
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="<?php echo base_url('wip');?>">Semanales</a></li>
+                                <li><a class="dropdown-item" href="<?php echo base_url('wip');?>">Mensuales</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url('about');?>">Nosotros</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url('commerce');?>">Como comprar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url('contact');?>">Contacto</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                <img class="icon-color-white"
+                                    src="<?php echo base_url("assets/bootstrap-icons-1.10.4/person-circle.svg");?>"
                                     alt="icono usuario" width="25" height="25">
                             </a>
                             <!--  <a class="nav-item dropdown-toggle" role="button"><img class="icon-color-white"
@@ -77,12 +195,85 @@
                                 </li>
                                 <li><a class="dropdown-item" href="<?php echo base_url('register');?>">Registrarse</a>
                                 </li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="<?php echo base_url('logout');?>">Salir</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+
+                <?php endif; ?>
+
+                <div class="collapse navbar-collapse " id="navbarNav">
+                    <ul class="navbar-nav ms-auto text-white ">
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url('');?>">Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Plato del dia
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="<?php echo base_url('products');?>">Carne</a></li>
+                                <li><a class="dropdown-item" href="<?php echo base_url('wip');?>">Vegetariano</a></li>
+                                <li><a class="dropdown-item" href="<?php echo base_url('wip');?>">Saludable</a></li>
+
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Viandas
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="<?php echo base_url('wip');?>">Semanales</a></li>
+                                <li><a class="dropdown-item" href="<?php echo base_url('wip');?>">Mensuales</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url('about');?>">Nosotros</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url('commerce');?>">Como comprar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url('contact');?>">Contacto</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                <img class="icon-color-white"
+                                    src="<?php echo base_url("assets/bootstrap-icons-1.10.4/person-circle.svg");?>"
+                                    alt="icono usuario" width="25" height="25">
+                            </a>
+                            <!--  <a class="nav-item dropdown-toggle" role="button"><img class="icon-color-white"
+                                    src="assets/bootstrap-icons-1.10.4/person-fill.svg" alt="icono facebook" width="25"
+                                    height="25" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i></a> -->
+
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="<?php echo base_url('login');?>">Ingresar</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="<?php echo base_url('register');?>">Registrarse</a>
+                                </li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="<?php echo base_url('logout');?>">Salir</a>
+                                </li>
                             </ul>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="<?php echo base_url('contact');?>"><img class="icon-color-white"
-                                    src="assets/bootstrap-icons-1.10.4/cart.svg" alt="icono carrito" width="25"
-                                    height="25">
+                                    src="<?php echo base_url("assets/bootstrap-icons-1.10.4/cart.svg");?>"
+                                    alt="icono carrito" width="25" height="25">
                             </a>
                         </li>
 
